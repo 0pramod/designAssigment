a2f27e71b4dfdbd2546a0eb56ab31f77a2c0d40a
0pramod <pramodhyadav072@gmail.com>
2022-06-27 08:54:04 +0545

    removed unnecessary files

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Assignment from "./components/assignment";
 
-import Kit from "./components/kit";
-// import Landing from "./components/landing";
-
 /**
  * App Router.
  */
@@ -12,7 +9,6 @@ function AppRouter() {
   return (
     <Router>
       <Route path="/" exact component={Assignment} />
-      <Route path="/docs" component={Kit} />
     </Router>
   );
 }
